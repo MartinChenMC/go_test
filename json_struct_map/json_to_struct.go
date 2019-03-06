421c65d8d74d11e2eb02ae846a7ccc49942248a8
martin <martin@lifesonic.cn>
2019-03-06 18:24:24 +0800

    Struct 和Json 相互转换

--- json_struct_map/json_to_struct.go
+++ json_struct_map/json_to_struct.go
@@ -18,21 +18,36 @@ type studentSlice struct {
 
 //var s Student
 var Ss studentSlice
+var Sarr studentSlice
 
 func JsonToStruct(str string) error  {
 	err := json.Unmarshal([]byte(str), &Ss)
 	return err
 }
 
-func main()  {
-	// int 类型的不能加双引号 否则结果为0
-	str := `{"student":[{"S_num":2,"S_name":"martin","S_addr":"martin_addr"},{"S_num":1,"S_name":"allen","S_addr":"allen_addr"}]}`
-	//str := `{"S_num":2,"S_name":"martin","S_addr":"martin_addr"}`
-	err := JsonToStruct(str)
-	if err != nil {
-		fmt.Println(err)
-		fmt.Println("json decode fail")
-	}
+func StructToJson()  {
+	Sarr.Student = append(Sarr.Student,Student{S_num:5,S_name:"martin",S_addr:"莱创讯"})
+	Sarr.Student = append(Sarr.Student,Student{S_num:6,S_name:"allen",S_addr:"莱创讯allen"})
 
-	fmt.Println(Ss);
+	jsonStr,err := json.Marshal(Sarr);
+	if err != nil {
+		fmt.Println("struct to json fail",err)
+	}
+	fmt.Println("struct to json",string(jsonStr))   //这里用string 强制转换类型，不然都是乱码
+}
+
+func main()  {
+
+	StructToJson()
+
+	// int 类型的不能加双引号 否则结果为0
+	//str := `{"student":[{"S_num":2,"S_name":"martin","S_addr":"martin_addr"},{"S_num":1,"S_name":"allen","S_addr":"allen_addr"}]}`
+	////str := `{"S_num":2,"S_name":"martin","S_addr":"martin_addr"}`
+	//err := JsonToStruct(str)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	fmt.Println("json decode fail")
+	//}
+	//
+	//fmt.Println(Ss);
 }
